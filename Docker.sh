671823165e71e7916195169f4437b8e2503f6c34
joseHbernardino <josehenrique9870@hotmail.com>
2022-10-03 05:07:20 -0300

    Finished V1.5

--- Docker.sh
+++ Docker.sh
@@ -3,12 +3,11 @@
 # Autor: José Henrique Nunes Bernardino
 # Contact: josehenrique9870@hotmail.com
 # Since: 23-09-2022
-# Version: v1.0
+# Version: v1.5
 
 
 # Variáveis utilizadas nas configurações do sistema
 DOCKERGPG="https://download.docker.com/linux/ubuntu/gpg"
-DOCKERKEY="0EBFCD88"
 DOCKERREP="deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 USUARIO=$(id -u)
@@ -25,8 +24,6 @@ libc-dev cargo make"
 # Variável de instalação do Docker Community CE.
 DOCKERINSTALL="docker-ce cgroup-lite docker-ce-cli"
 
-# Porta padrão do Portainer.io
-PORTPORTAINER="9000"
 
 # Configuração da variável de log
 LOG=("/var/log/dockerinstall")
@@ -57,25 +54,14 @@ elif [ "$USUARIO" == '0' ]
         exit 1
 fi
 
-#
-# Verificação da porta 9000
-#
-if [ "$(nc -vz 127.0.0.1 $PORTPORTAINER &> /dev/null ; echo $?)" == "0" ]
-	then
-        echo -e "A porta $PORTPORTAINER ja está sendo utilizada."
-        sleep 2
-        exit 1
-    else
-        echo -e "A porta $PORTPORTAINER não está sendo utilizada."
-        sleep 2
-fi
 
+echo -e "Início do script $0 em: $(date +%d/%m/%Y-"("%H:%M")")\n\n" &>> $LOG
 
 #
 # Verificação de dependências do Docker Community
 #
 
-echo -n "Verificação de dependencias do Docker Community, aguarde..."
+echo -n "Verificação de dependencias do Docker Community..."
     for name in $DOCKERDEP
     do
         [[ $(dpkg -s $name 2> /dev/null) ]] || {
@@ -90,77 +76,49 @@ echo -n "Verificação de dependencias do Docker Community, aguarde..."
         sleep 3
 
 
-#
-# Instalação do Bind9 e do Portainer.io
-#
 
-echo -e "Início do script $0 em: $(date +%d/%m/%Y-"("%H:%M")")\n" &>> $LOG
+echo -e "Atualizando o sistema.\n"
 
-echo 
+    apt-get update && apt-get upgrade -y  &>> $LOG
 
-echo -e "Após instalação do Portainer.io acesse: http://$(hostname -d | cut -d ' ' -f1):9000/\n"
-sleep 3
-
-#
-# Atualização de pacotes
-#
-
-echo -e "Atualizando todo o sistema operacional, aguarde..."
-
-echo 
-
-    apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get full-upgrade -y &>> $LOG
-
-echo -e "Sistema atualizado com sucesso!!!, continuando...\n"
+echo -e "Sistema atualizado com sucesso.\n"
 
 
-echo -e "Removendo todos os software desnecessários, aguarde..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	# opção do comando apt: -y (yes)
+echo -e "Removendo todos os software desnecessários."
 	apt -y autoremove &>> $LOG
 	apt -y autoclean &>> $LOG
-echo -e "Software removidos com sucesso!!!, continuando...\n"
+echo -e "Software removidos com sucesso.\n"
+
 
 #
 # Instalação de pacotes necessarios para o Docker
 #
 
-echo -e "Incio de instalação de pacotes necessarios, aguarde..."
 
-echo
+echo -e "Incio de instalação de pacotes necessarios..."
+	apt-get install $DOCKERINSTALLDEP -y &>> $LOG
+echo -e "Instalação das dependências feita com sucesso.\n"
+sleep 2
 
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
 
-echo -e "Adicionando a Chave GPG do Docker, aguarde..."
+echo -e "Adicionando a Chave GPG do Docker..."
 	curl -fsSL $DOCKERGPG | apt-key add - &>> $LOG
-echo -e "Chave adicionada com sucesso!!!, continuando...\n"
+echo -e "Chave adicionada com sucesso.\n"
 
 
-echo -e "Adicionando o repositório do Docker, aguarde..."
+echo -e "Adicionando o repositório do Docker..."
 	add-apt-repository "$DOCKERREP" &>> $LOG
-echo -e "Repositório adicionado com sucesso!!!, continuando...\n"
+echo -e "Repositório adicionado com sucesso.\n"
 sleep 1
 
 
-echo -e "Atualizando list APT, aguarde..."
-
-echo 
-
-    apt-get update -y &>> $LOG
-
-echo -e "Sistema atualizado com sucesso!!!, continuando...\n"
-
-
-echo -e "Instalando o Docker CE, aguarde..."
-	apt  install $DOCKERINSTALL -y &>> $LOG
-echo -e "Docker instalado com sucesso!!!, continuando...\n"
+echo -e "Instalando o Docker CE..."
+	apt-get  install $DOCKERINSTALL -y &>> $LOG
+echo -e "Docker instalado com sucesso.\n"
 sleep 2
 
-echo -e "Instalando o Docker Compose, aguarde..."
+
+echo -e "Instalando o Docker Compose..."
     wget -O docker-compose $DOCKERCOMPOSE &>> $LOG
     mv docker-compose /usr/local/bin/ &>> $LOG
     chmod +x /usr/local/bin/ &>> $LOG
@@ -172,9 +130,37 @@ echo -e "Usuario Root adicionado no grupo do Docker."
     usermod -aG docker $USER &>> $LOG
 sleep 1
 
-echo -e "Habilitando inicialização do Docker "
-    systemctl enable docker $>> $LOG
-    systemctl start docker $>> $LOG
+echo -e "Habilitando inicialização do Docker \n"
+    systemctl enable docker &>> $LOG
+    /etc/init.d/docker start docker &>> $LOG
 sleep 1
 
 
+echo -e "\n------------------------------"
+echo -e "| Configuração do Portainer.io|"
+echo -e "------------------------------\n"
+
+
+
+docker volume create portainer_data &>> $LOG
+echo -e "Volume do Portainer.io                -   \033[01;32m OK \033[01;37m"
+sleep 1
+
+docker run --name portainer -d -p 9000:9000 --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer &>> $LOG
+echo -e "Criação do Container                  -   \033[01;32m OK \033[01;37m"
+sleep 2
+
+
+
+echo -e "Criação do Serviço de Inicialização"
+    systemctl daemon-reload &>> $LOG
+    systemctl enable portainer &>> $LOG
+    systemctl start portainer &>> $LOG
+sleep 2
+
+echo -e "Fim do script $0 em: $(date +%d/%m/%Y-"("%H:%M")")\n"
+echo -e "Instalação concluida com sucesso press <Enter>"
+
+
+echo -e "Após instalação do Portainer.io acesse: http://localhost:9000/\n"
+sleep 3
